51451787245594ba73bb733262bb42313b037ba0
jesse.langat@student.moringaschool.com <jesse.langat@student.moringaschool.com>
2024-04-15 16:09:01 +0300

    go

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  let form = document.querySelector("form")
+  form.addEventListener("submit", function(e){
+    e.preventDefault();
+    newP(e.target.new_task_description.value)
+    form.reset()
+  })
+})
+
+function newP(toDoEnteredStoredHere){
+  let newUl = document.createElement('ul')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', deleteToDoAndAddToCompletedList)
+  btn.textContent = ' 💖'
+  newUl.textContent= `${toDoEnteredStoredHere} `
+  newUl.appendChild(btn)
+  document.querySelector('#tasks').appendChild(newUl) 
+}
+function deleteToDoAndAddToCompletedList(e){
+  taskItem = e.target.parentNode
+  taskItem.remove();
+  document.querySelector('#tasks_completed').appendChild(taskItem)
+}
